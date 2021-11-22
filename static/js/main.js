b19a5915baecf8a3ad0f57c95da1310c2cbd03e0
ilya.nikitaev <ilya.nikitaev@natlex.ru>
2021-11-22 20:12:47 +0300

    markup

--- static/js/main.js
+++ static/js/main.js
@@ -15,3 +15,21 @@ function getImgModal(e) {
     $('#modalImg__img').attr('src', urlImg);
     $('#modalImg').modal('show');
 }
+
+$('#camera-modal-link').bind('click', function () {
+    $('#camera-modal').modal('show');
+})
+
+// Код для модалок
+// $("a[data-toggle='modal']").bind("click", function () {
+//     var id = $(this).attr('data-id');
+//     $.ajax({
+//         url: "test",
+//         cache: false,
+//         data: {id: id},
+//         type: "GET",
+//         success: function (html) {
+//             $('.modal__content').html(html);
+//         }
+//     });
+// });
